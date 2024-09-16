24ec46c295dde1c9049e21181c38a37ccf44fb2d
Achimas <agafonovrn.it@gmail.com>
2024-09-16 14:31:39 +0700

    Update toppest upgrades buying

--- clicker.py
+++ clicker.py
@@ -18,7 +18,7 @@ def send_to_TG(text, chat_id = None, bot_token = None):
             print (text))
     except Exception: print (Exception)
 
-def buy_best_upgrade():
+def _take_toppest_upgrades():
     headers = {
         'User-Agent': 'Mozilla/5.0 (Android 12; Mobile; rv:102.0) Gecko/102.0 Firefox/102.0',
         'Accept': '*/*',
@@ -40,7 +40,7 @@ def buy_best_upgrade():
         if not item["isExpired"] and item["isAvailable"] and item["price"] > 0
     ]
 
-    toppest_upgrades = [["", int(0), "0"]]
+    best_upgrade_list = [["", int(0), "0"]]
     for i in upgrades:
         try:
             id = i["id"]
@@ -52,16 +52,19 @@ def buy_best_upgrade():
         try:
             if efficiency_coeff > 0.003:
                 top_up = [id, efficiency_coeff, price]
-                toppest_upgrades.append(top_up)
+                best_upgrade_list.append(top_up)
         except Exception as e:
             print(f"Sth happens wrong in adding to list:\n'{i}'")
             print(e)
-    formatted_date = time.strftime("%d.%m.%Y %H:%M", time. localtime())
-    toppest_upgrades.pop(0)
-    print (toppest_upgrades)
-    sorted_upgrades = sorted(toppest_upgrades, key=lambda x: x[1])
+    best_upgrade_list.pop(0)
+    sorted_upgrades = sorted(best_upgrade_list, key=lambda x: x[1])
     print (sorted_upgrades)
-    for up in sorted_upgrades:
+    return sorted_upgrades
+
+def buy_best_upgrade():
+    toppest_upgrades = _take_toppest_upgrades()
+    
+    for up in toppest_upgrades:
         upgrade_id = up[0]
         headers = {
             "Content-Type": "application/json",
@@ -80,13 +83,10 @@ def buy_best_upgrade():
             send_to_TG(e, chat_id=chat_id,
                         bot_token=bot_token)
         if response and response.status_code == 200:
-            formatted_date = time.strftime("%d.%m.%Y %H:%M", time.localtime())
             price = f"{up[2]:,}".replace(",", "'")
-            text = f"[{formatted_date}]\nUpgrade {up[0]} is purcashed.\nPrice - <b>{price}</b>"
+            text = f"Upgrade {up[0]} is purcashed.\nPrice - <b>{price}</b>"
             send_to_TG(text, chat_id=chat_id,
                         bot_token=bot_token)
-#               if response.status_code == 400:
-#                  print(f"Upgrade {up[0]} cannot be percashed. Price {up[2]}")
 
 def get_avaliable_taps():
     # Получаем количество доступных тапов
@@ -105,23 +105,7 @@ def get_avaliable_taps():
         taps = 14
     return taps
 
-# загрузка из переменных окружения
-load_dotenv()
-chat_id = os.getenv("chat_id")
-bot_token = os.getenv("bot_token")
-authorization = os.getenv("authorization")
-# В моем случае предел равен 7000
-try: load_capacity =  os.getenv("capacity")
-except: load_capacity = None
-capacity = int(load_capacity)-2000 if load_capacity else 900
-
-while True:
-    # Получаем количество доступных тапов
-    taps = get_avaliable_taps()
-    count = int(random.uniform(0, taps))
-
-    time.sleep(1)
-
+def make_taps(taps, count):
     # Отправляем запрос для выполнения тапов
     try:
         tap_response = requests.post(
@@ -135,17 +119,33 @@ while True:
             'count': count,
             'timestamp': int(time.time())
         }
-    )
-    except Exception as e:
-        send_to_TG (e, chat_id = chat_id, bot_token = bot_token)
-    try:
-        if tap_response.status_code == 200:
-            formatted_date = time.strftime("%d.%m.%Y %H:%M", time.localtime())
+        )
+        if isinstance(tap_response, requests.Response) and tap_response.status_code == 200:
             raw_balance = int(tap_response.json().get('clickerUser', {}).get("balanceCoins", 0))+count
             balance = f"{raw_balance:,}".replace(",", "'")
-            send_to_TG (f"[{formatted_date}]\nResponse on {count} taps successfully posted.\nBalance - <b>{balance}</b>", chat_id = chat_id, bot_token = bot_token)
-    except Exception:
-        print(Exception)
+            text = f"Response on {count} taps successfully posted.\nBalance - <b>{balance}</b>"
+            return text
+        else: text = "Taps not posted"
+    except Exception as e:
+        text = repr(e)
+    return text
+
+# загрузка из переменных окружения
+load_dotenv()
+chat_id = os.getenv("chat_id")
+bot_token = os.getenv("bot_token")
+authorization = os.getenv("authorization")
+
+while True:
+    # Получаем количество доступных тапов
+    taps = get_avaliable_taps()
+    count = int(random.uniform(0, taps))
+
+    time.sleep(1)
+
+    taps_result = make_taps(taps, count)
+    send_to_TG(taps_result, chat_id = chat_id, bot_token = bot_token)
+
     time.sleep(1)
     buy_best_upgrade()
     time.sleep(589)
